1234933eb1560cd075205f2e9846773f37641098
Maxime Deboffle <maxime.deboffle@hotmail.com>
2019-10-10 16:02:44 +0200

    fdp

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
   entry : './src/main.js',
   output: {
     path:path.resolve(__dirname, "dist"),
-    filename: 'main.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
